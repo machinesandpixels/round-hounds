0f759a5413f54e5061a407eb6dfd30cc2a5161ef
jesusquezada <2016quezadaj@gmail.com>
2020-10-23 23:10:01 -0700

    Implemented itemScreen layout with image, price, toppings and add to cart button

--- frontend/src/screens/ItemScreen.js
+++ frontend/src/screens/ItemScreen.js
@@ -1,10 +1,74 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import {Button, Card, Col, Image, ListGroup, Row} from 'react-bootstrap';
+import Rating from '../components/Rating';
+import menuItems from '../menuItems';
 
-const ItemScreen = () => {
+const ItemScreen = ({ match }) => {
+    const item = menuItems.find(item => item._id == match.params.id);
+  
     return (
-        <div>
-            Item
-        </div>
+        <Fragment>
+            <Link to="/" className="btn btn-light my-3"> Go Back</Link>
+            <Row>
+                <Col md={3}>
+                    <Image src={item.image} alt={item.name} fluid>
+                    </Image>
+                </Col>
+                <Col md={6}>
+                    <ListGroup variant="flush">
+                        <ListGroup.Item>
+                            <h3>{item.name}</h3>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Rating 
+                                value={item.rating} 
+                                text={`${item.numReviews} reviews`} 
+                            />
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Price: ${item.price}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Toppings: {item.toppings}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col md={3}>
+                    <Card>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>
+                                        Price:
+                                    </Col>
+                                    <Col>
+                                        <strong>
+                                            ${item.price}
+                                        </strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>
+                                        Status
+                                    </Col>
+                                    <Col>
+                                        {item.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Button className="btn-block" type="button" disabled={item.countInStock === 0}>
+                                    Add To Cart
+                                </Button>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+        </Fragment>
     )
 }
 
