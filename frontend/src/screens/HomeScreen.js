ff9974d8ff3945e336c144a8e1edb98c74c694ba
jesusquezada <2016quezadaj@gmail.com>
2020-10-28 23:59:20 -0700

    Started to fetch data from our backend server using axios

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,23 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
 import { Row, Col }from 'react-bootstrap';
 import Item from '../components/Item';
+
 import menuItems from '../menuItems';
+const endPoint = 'http://localhost:5000/api/v1/menu';
 
 const HomeScreen = () => {
+   const [menu, setMenu] = useState([]);
+
+   useEffect(() => {
+        const fetchMenu = async () => {
+            const { data } = await axios.get(endPoint);
+            setMenu(menu);
+        }
+
+        fetchMenu();
+   }, []);
+
     return (
         <Fragment>
             <h2 className="text-center">Menu</h2>
